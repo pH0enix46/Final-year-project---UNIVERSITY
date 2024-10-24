5a733f14a11cc2b0f3f3e58ce5c2fe4a7f32f98e
pH0enix46 <mdjoy.javascript@gmail.com>
2024-10-24 11:33:00 +0600

    ✅ add theme and create some basic navbar

--- FRONTEND/src/components/Navbar.jsx
+++ FRONTEND/src/components/Navbar.jsx
@@ -4,10 +4,36 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <img src={assets.logo} alt="mac-haven--logo" className="w-16" />
+      <img src={assets.logo} alt="mac-haven--logo" className="w-20" />
 
-      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
-        <li></li>
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-300">
+        <NavLink to="/" className="flex flex-col items-center gap-1 font-bold">
+          <span className="relative hover:bg-gray-400 px-2 py-1.5 transition-all rounded-md border-2 border-transparent hover:border-gray-300 hover:text-gray-700">
+            HOME
+          </span>
+          <hr className="w-3/5 border-none h-[2px] bg-gray-300" />
+        </NavLink>
+
+        <NavLink to="/" className="flex flex-col items-center gap-1 font-bold">
+          <span className="relative hover:bg-gray-400 px-2 py-1.5 transition-all rounded-md border-2 border-transparent hover:border-gray-300 hover:text-gray-700">
+            COLLECTION
+          </span>
+          <hr className="w-3/5 border-none h-[2px] bg-gray-300" />
+        </NavLink>
+
+        <NavLink to="/" className="flex flex-col items-center gap-1 font-bold">
+          <span className="relative hover:bg-gray-400 px-2 py-1.5 transition-all rounded-md border-2 border-transparent hover:border-gray-300 hover:text-gray-700">
+            ABOUT
+          </span>
+          <hr className="w-3/5 border-none h-[2px] bg-gray-300" />
+        </NavLink>
+
+        <NavLink to="/" className="flex flex-col items-center gap-1 font-bold">
+          <span className="relative hover:bg-gray-400 px-2 py-1.5 transition-all rounded-md border-2 border-transparent hover:border-gray-300 hover:text-gray-700">
+            CONTACT
+          </span>
+          <hr className="w-3/5 border-none h-[2px] bg-gray-300" />
+        </NavLink>
       </ul>
     </div>
   );
