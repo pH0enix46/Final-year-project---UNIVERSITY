b328f3897e882277f05ed1edf1570845fec8e0ac
pH0enix46 <mdjoy.javascript@gmail.com>
2024-10-24 10:38:59 +0600

    🌠 create and add brand logo

--- FRONTEND/src/assets/assets.js
+++
@@ -1,3 +0,0 @@
-export const assets = {};
-
-export const products = [];
--- FRONTEND/src/components/Navbar.jsx
+++ FRONTEND/src/components/Navbar.jsx
@@ -1,4 +1,15 @@
+import { assets } from "../assets/frontend-assets/assets";
+import { NavLink } from "react-router-dom";
+
 function Navbar() {
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-between py-5 font-medium">
+      <img src={assets.logo} alt="mac-haven--logo" className="w-16" />
+
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+        <li></li>
+      </ul>
+    </div>
+  );
 }
 export default Navbar;
