89a6739d8644babd47c9623902e5b2801b60b881
pH0enix46 <mdjoy.javascript@gmail.com>
2024-10-24 14:33:01 +0600

    💻 build cool navbar for desktop

--- FRONTEND/src/components/Navbar.jsx
+++ FRONTEND/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import { assets } from "../assets/frontend_assets/assets";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 function Navbar() {
   return (
-    <div className="flex items-center justify-between py-5 font-medium">
-      {/* ✅ LOGO */}
+    <div className="flex items-center justify-between font-medium border-2 p-4 rounded-2xl border-secondary shadow-xl">
+      {/* ⏺ LOGO */}
       <div
         className={`relative p-1 transition-all duration-[.3s] border-4 border-transparent hover:border-gray-400 hover:shadow-lg hover:animate-pulse rounded-full ${styles.glow}`}
       >
@@ -15,9 +15,9 @@ function Navbar() {
           className="w-20 rounded-full shadow-2xl cursor-pointer border-2 border-gray-400"
         />
       </div>
-      {/* ⛔️ LOGO END */}
+      {/* ⏺ LOGO END */}
 
-      {/* ✅ NAV */}
+      {/* ⏺ NAV */}
       <ul className="hidden sm:flex gap-5 text-sm text-gray-300">
         <NavLink to="/" className="flex flex-col items-center gap-1 font-bold">
           <span className="relative hover:bg-gray-400 px-2 py-1.5 transition-all rounded-md border-2 border-transparent hover:border-gray-300 hover:text-gray-700">
@@ -56,9 +56,9 @@ function Navbar() {
           <hr className="w-3/5 border-none h-[2px] bg-gray-300 hidden transition-all" />
         </NavLink>
       </ul>
-      {/* ⛔️ NAV END */}
+      {/* ⏺ NAV END */}
 
-      {/* ✅ OTHERS */}
+      {/* ⏺ OTHERS */}
       <div className="flex items-center gap-6">
         <div className="p-2 rounded-full bg-gray-300 bg-opacity-30 backdrop-blur-md flex items-center justify-center shadow-lg border-4 border-gray-400">
           <img
@@ -93,8 +93,21 @@ function Navbar() {
             </li>
           </ul>
         </div>
+
+        <Link to="/cart" className="relative">
+          <div className="p-2 rounded-full bg-gray-300 bg-opacity-30 backdrop-blur-md flex items-center justify-center shadow-lg border-4 border-gray-400">
+            <img
+              src={assets.cart_icon}
+              alt="cart_icon"
+              className="w-5 h-5 cursor-pointer"
+            />
+            <p className="absolute right-[-8px] bottom-[-8px] w-6 h-6 text-[14px] text-center leading-5 bg-primary text-slate-300 aspect-square rounded-full border-2 border-gray-400">
+              4
+            </p>
+          </div>
+        </Link>
       </div>
-      {/* ⛔️ OTHER'S END */}
+      {/* ⏺ OTHER'S END */}
     </div>
   );
 }
