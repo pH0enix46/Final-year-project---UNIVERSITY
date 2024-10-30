f8e42076ee6129b50800a0a06db3bdc6a8f648f4
pH0enix46 <mdjoy.javascript@gmail.com>
2024-10-31 00:13:38 +0600

    ⭐ add sort part

--- FRONTEND/src/components/AllCollection.jsx
+++ FRONTEND/src/components/AllCollection.jsx
@@ -7,6 +7,14 @@ function AllCollection() {
         <span className="lg:ml-8 md:ml-4">
           <Title text1={"ALL"} text2={"COLLECTION"} />
         </span>
+
+        {/* ⏺ SORT */}
+        <select className="select select-bordered w-auto bg-brand border-2 shadow-md">
+          <option value={"Default"}>Sort by-- Default</option>
+          <option value={"Low > High"}>Sort by-- Low &gt; High</option>
+          <option value={"High > Low"}>Sort by-- High &gt; Low</option>
+        </select>
+        {/* ⏺ SORT END */}
       </div>
     </div>
   );
