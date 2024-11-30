6361a47c42ab2f965af199b7ea614b34116d50fe
pH0enix46 <mdjoy.javascript@gmail.com>
2024-11-30 10:56:41 +0600

    📦 about section done

--- FRONTEND/src/pages/About.jsx
+++ FRONTEND/src/pages/About.jsx
@@ -49,8 +49,8 @@ function About() {
           <Title text1={"WHY"} text2={"CHOOSE US"} />
         </div>
 
-        <div className="text-lg flex flex-col gap-14">
-          <div className="border-2 m-3 p-3 rounded-xl shadow-xl border-secondary">
+        <div className="text-lg flex flex-col">
+          <div className="border-2 m-3 p-3 rounded-xl shadow-lg border-secondary">
             <h3 className="text-2xl border-b-2 border-b-gray-400 inline-block mb-2">
               The Main Goal and Aim
             </h3>
@@ -64,6 +64,59 @@ function About() {
               prioritizing innovation, exceptional service, and customer
               satisfaction, we strive to make cutting-edge technology accessible
               and impactful for everyone
+            </p>
+          </div>
+
+          <div className="border-2 m-3 p-3 rounded-xl shadow-lg border-secondary">
+            <h3 className="text-2xl border-b-2 border-b-gray-400 inline-block mb-2">
+              Customer Satisfaction
+            </h3>
+            <p>
+              At <strong className="text-xl italic">Machaven</strong>, your
+              satisfaction is our highest priority. We understand that
+              purchasing a MacBook is a significant investment, and we are
+              committed to making the process as seamless and enjoyable as
+              possible. From offering the latest models at competitive prices to
+              providing expert guidance throughout your buying journey, we
+              ensure that every customer receives personalized attention and
+              care. We believe that a happy customer is our best advertisement,
+              and we go above and beyond to ensure that you leave satisfied.
+              When you choose Machaven, you’re not just buying a MacBook; you’re
+              joining a community committed to supporting your needs long after
+              the sale
+            </p>
+          </div>
+
+          <div className="border-2 m-3 p-3 rounded-xl shadow-lg border-secondary">
+            <h3 className="text-2xl border-b-2 border-b-gray-400 inline-block mb-2">
+              The Brand That Cares For You
+            </h3>
+            <p>
+              At <strong className="text-xl italic">Machaven</strong>, we’re not
+              just a store; we’re a brand that truly cares about your needs. We
+              understand that when it comes to technology, it’s not just about
+              the product – it’s about your experience, your satisfaction, and
+              your peace of mind. Whether you’re a student, professional, or
+              creative, Machaven is dedicated to providing you with a
+              hassle-free, enjoyable experience. We take pride in delivering not
+              just exceptional products, but also personalized service that
+              ensures you feel heard, valued, and supported long after your
+              purchase
+            </p>
+          </div>
+
+          <div className="border-2 m-3 p-3 rounded-xl shadow-lg border-secondary">
+            <h3 className="text-2xl border-b-2 border-b-gray-400 inline-block mb-2">
+              Dedicated Service Center
+            </h3>
+            <p>
+              At <strong className="text-xl italic">Machaven</strong>, we’re
+              proud to offer a Dedicated Service Center designed to provide the
+              best after-sales support for your MacBook. Our expert technicians
+              are committed to ensuring your device continues to perform at its
+              best long after your purchase. Whether it’s troubleshooting,
+              repairs, or maintenance, we provide fast and reliable solutions
+              tailored to your needs
             </p>
           </div>
         </div>
