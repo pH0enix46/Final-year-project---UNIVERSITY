a76351f25fda0c32916439a8fa1f2f57c9eb7e9c
pH0enix46 <mdjoy.javascript@gmail.com>
2025-02-16 07:35:41 +0600

    working with place-order -- left-side

--- FRONTEND/src/pages/PlaceOrder.jsx
+++ FRONTEND/src/pages/PlaceOrder.jsx
@@ -8,6 +8,43 @@ function PlaceOrder() {
         <div className="text-xl sm:text-2xl my-3">
           <Title text1={"DELIVERY"} text2={"INFORMATION"} />
         </div>
+
+        <div className="border border-gray-600 px-6 py-12 rounded-xl shadow-lg shadow-gray-500 flex flex-col gap-6">
+          <div className="flex gap-3">
+            <input
+              type="text"
+              placeholder="First Name"
+              className="border-2 border-gray-500 rounded-lg py-1 px-2 w-full bg-transparent text-gray-700 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent transition-all duration-300 ease-in-out"
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              className="border-2 border-gray-500 rounded-lg py-1 px-2 w-full bg-transparent text-gray-700 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent transition-all duration-300 ease-in-out"
+            />
+          </div>
+          <input
+            type="email"
+            placeholder="Email address"
+            className="border-2 border-gray-500 rounded-lg px-2 w-full bg-transparent text-gray-700 placeholder-gray-500 py-1 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent transition-all duration-300 ease-in-out"
+          />
+          <div className="flex gap-3">
+            <input
+              type="text"
+              placeholder="City"
+              className="border-2 border-gray-500 rounded-lg py-1 px-2 w-full bg-transparent text-gray-700 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent transition-all duration-300 ease-in-out"
+            />
+            <input
+              type="text"
+              placeholder="Country"
+              className="border-2 border-gray-500 rounded-lg py-1 px-2 w-full bg-transparent text-gray-700 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent transition-all duration-300 ease-in-out"
+            />
+          </div>
+          <input
+            type="number"
+            placeholder="Phone number"
+            className="border-2 border-gray-500 rounded-lg px-2 w-full bg-transparent text-gray-700 placeholder-gray-500 py-1 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent transition-all duration-300 ease-in-out"
+          />
+        </div>
       </div>
     </div>
   );
