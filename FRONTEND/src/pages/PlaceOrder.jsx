d701d4cb429900101ec7af2873d542dc0e1e8621
pH0enix46 <mdjoy.javascript@gmail.com>
2025-02-16 07:14:37 +0600

    working with place-order .. ✅

--- FRONTEND/src/pages/PlaceOrder.jsx
+++ FRONTEND/src/pages/PlaceOrder.jsx
@@ -1,4 +1,15 @@
+import Title from "./../components/Title";
+
 function PlaceOrder() {
-  return <div className="flex flex-col"></div>;
+  return (
+    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh]">
+      {/* ------------------ LEFT SIDE ------------------ */}
+      <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
+        <div className="text-xl sm:text-2xl my-3">
+          <Title text1={"DELIVERY"} text2={"INFORMATION"} />
+        </div>
+      </div>
+    </div>
+  );
 }
 export default PlaceOrder;
