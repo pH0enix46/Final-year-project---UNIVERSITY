308e2cc0ad6f49561f0ae57b3f697e1095b68548
pH0enix46 <mdjoy.javascript@gmail.com>
2025-02-16 08:27:16 +0600

    working with place-order -- right-side

--- FRONTEND/src/assets/frontend_assets/assets.js
+++ FRONTEND/src/assets/frontend_assets/assets.js
@@ -13,6 +13,7 @@ import support from "./support.png";
 import cross_icon from "./cross_icon.png";
 import bin_icon from "./bin_icon.png";
 import aboutUs_image from "./about-us_image.png";
+import stripe_logo from "./stripe_logo.png";
 
 import mac_img01 from "./products/mac-1.webp";
 import mac_img02 from "./products/mac-2.webp";
@@ -42,6 +43,7 @@ export const assets = {
   cross_icon,
   bin_icon,
   aboutUs_image,
+  stripe_logo,
 };
 
 export const products = [
--- FRONTEND/src/pages/PlaceOrder.jsx
+++ FRONTEND/src/pages/PlaceOrder.jsx
@@ -1,15 +1,20 @@
 import Title from "./../components/Title";
+import CartTotal from "./../components/CartTotal";
+import { assets } from "../assets/frontend_assets/assets";
+import { useState } from "react";
 
 function PlaceOrder() {
+  const [method, setMethod] = useState("CashOnDelivery");
+
   return (
-    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh]">
+    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] items-start">
       {/* ------------------ LEFT SIDE ------------------ */}
       <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
         <div className="text-xl sm:text-2xl my-3">
           <Title text1={"DELIVERY"} text2={"INFORMATION"} />
         </div>
 
-        <div className="border border-gray-600 px-6 py-12 rounded-xl shadow-lg shadow-gray-500 flex flex-col gap-6">
+        <div className="border-2 border-gray-500 px-6 py-12 rounded-xl shadow-lg shadow-gray-500 flex flex-col gap-6">
           <div className="flex gap-3">
             <input
               type="text"
@@ -44,6 +49,48 @@ function PlaceOrder() {
             placeholder="Phone number"
             className="border-2 border-gray-500 rounded-lg px-2 w-full bg-transparent text-gray-700 placeholder-gray-500 py-1 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent transition-all duration-300 ease-in-out"
           />
+        </div>
+      </div>
+
+      {/* ------------------ RIGHT SIDE ------------------ */}
+      <div className="">
+        <div className="mt-8 min-w-80">
+          <CartTotal />
+        </div>
+
+        <div className="mt-12">
+          <Title text1={"PAYMENT"} text2={"METHOD"} />
+          <div className="flex gap-5 flex-col md:flex-row">
+            <div
+              onClick={() => setMethod("Stripe")}
+              className="flex items-center gap-3 border border-gray-500 rounded-md p-2 px-3 cursor-pointer shadow-xl"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border-2 border-gray-400 rounded-full ${
+                  method === "Stripe" ? "bg-green-500" : ""
+                }`}
+              ></p>
+              <img src={assets.stripe_logo} alt="stripe" className="h-6 mx-2" />
+            </div>
+
+            <div
+              onClick={() => setMethod("CashOnDelivery")}
+              className="flex items-center gap-3 border border-gray-500 rounded-md p-2 px-3 cursor-pointer shadow-xl"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border-2 border-gray-400 rounded-full ${
+                  method === "CashOnDelivery" ? "bg-green-500" : ""
+                }`}
+              ></p>
+              <p className="text-sm font-medium">CASH ON DELIVERY</p>
+            </div>
+          </div>
+
+          <div className="w-full text-end mt-8">
+            <button className="bg-transparent px-10 py-3 border-2 border-gray-500 rounded-lg font-semibold transition-all duration-300 ease-in-out hover:bg-gray-600 hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-50">
+              Place Order!
+            </button>
+          </div>
         </div>
       </div>
     </div>
