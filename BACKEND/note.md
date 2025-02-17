5022b47bb28d8d73891d3a541f0c9428c17d1d59
pH0enix46 <mdjoy.javascript@gmail.com>
2025-02-18 02:36:21 +0600

    start the server.....✅

--- BACKEND/note.md
+++ BACKEND/note.md
@@ -2,3 +2,8 @@
 
 - run `npm init`
 - run `npm i cors dotenv express jsonwebtoken mongoose multer nodemon stripe validator cloudinary bcrypt`
+
+- change scripts to `"server": "nodemon server.js"`
+- chnage `"type": "module",` but default commonjs, here i just try it in diff way..
+
+- `npm run server`
--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,1 +1,21 @@
 //
+
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+// APP CONFIG ----------------//
+const app = express();
+const port = process.env.PORT || 4000;
+
+// MIDDLEWARES ---------------//
+app.use(express.json());
+app.use(cors());
+
+// API ENDPOINTS --------------//
+app.get("/", (req, res) => {
+  res.send("API Working!!...");
+});
+
+// START SERVER ------------//
+app.listen(port, () => console.log(`Server started on PORT: ${port}`));
