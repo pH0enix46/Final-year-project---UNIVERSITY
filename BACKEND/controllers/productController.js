fbf0e201889bc0066a6c5b760bd0224c3c0fa60d
pH0enix46 <mdjoy.javascript@gmail.com>
2025-04-14 22:25:51 +0600

    create full logic of pruct card added also unig cloudinary for hosting our images to emplement this we use multer middleware

--- BACKEND/controllers/productController.js
+++ BACKEND/controllers/productController.js
@@ -1,8 +1,74 @@
 // // //
+import { v2 as cloudinary } from "cloudinary";
+import productModel from "../models/productModel.js";
 
 // function for add product
 export async function addProduct(req, res) {
+  try {
+    const {
+      name,
+      description,
+      price,
+      category,
+      subCategory,
+      colors,
+      bestseller,
+    } = req.body;
 
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
+
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item !== undefined
+    );
+
+    let imagesUrl = await Promise.all(
+      images.map(async (image) => {
+        let result = await cloudinary.uploader.upload(image.path, {
+          resource_type: "image",
+        });
+
+        return result.secure_url;
+      })
+    );
+
+    // console.log(
+    //   name,
+    //   description,
+    //   price,
+    //   category,
+    //   subcategory,
+    //   colors,
+    //   bestseller
+    // );
+    // console.log(imagesUrl);
+
+    const productData = {
+      name,
+      description,
+      price: +price,
+      category,
+      subCategory,
+      bestseller: bestseller === "true" ? true : false,
+      colors: JSON.parse(colors),
+      image: imagesUrl,
+      date: Date.now(),
+    };
+    console.log(productData);
+
+    const product = new productModel(productData);
+    await product.save();
+
+    res.json({ success: true, message: "Product added!" });
+  } catch (error) {
+    console.error(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
 }
 
 // function for list product
--- BACKEND/routes/productRoute.js
+++ BACKEND/routes/productRoute.js
@@ -6,10 +6,20 @@ import {
   removeProduct,
   singleProductInfo,
 } from "../controllers/productController.js";
+import upload from "../middleware/multer.js";
 
 const pruductRouter = express.Router();
 
-pruductRouter.post("/add", addProduct);
+pruductRouter.post(
+  "/add",
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  addProduct
+);
 pruductRouter.post("/remove", removeProduct);
 pruductRouter.post("/singleProductInfo", singleProductInfo);
 pruductRouter.get("/list", listProduct);
