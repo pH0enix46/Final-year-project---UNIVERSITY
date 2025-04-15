3aa070a354dd1ab1f1c7789afd7e292ba43235f3
pH0enix46 <mdjoy.javascript@gmail.com>
2025-04-15 06:40:41 +0600

    create all the product logic and successfully test that all out

--- BACKEND/controllers/productController.js
+++ BACKEND/controllers/productController.js
@@ -72,10 +72,54 @@ export async function addProduct(req, res) {
 }
 
 // function for list product
-export async function listProduct(req, res) {}
+export async function listProduct(req, res) {
+  try {
+    const products = await productModel.find({});
+
+    res.json({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
 
 // function for remove product
-export async function removeProduct(req, res) {}
+export async function removeProduct(req, res) {
+  try {
+    await productModel.findByIdAndDelete(req.body.id);
+    res.json({
+      success: true,
+      message: "Product removed!",
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
 
 // function for single product infp
-export async function singleProductInfo(req, res) {}
+export async function singleProductInfo(req, res) {
+  try {
+    const { productId } = req.body;
+    const product = await productModel.findById(productId);
+    res.json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
