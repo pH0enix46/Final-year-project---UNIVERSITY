b37469a2f840ba17e7414f36e520111a150130ee
pH0enix46 <mdjoy.javascript@gmail.com>
2025-05-11 12:13:21 +0600

    changes the logic for stripe

--- BACKEND/controllers/orderController.js
+++ BACKEND/controllers/orderController.js
@@ -2,6 +2,10 @@ import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 import Stripe from "stripe";
 
+// global variables
+const currency = "BDT";
+const deliveryCharge = 50;
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
@@ -32,6 +36,7 @@ const placeOrder = async (req, res) => {
 const placeOrderStripe = async (req, res) => {
   try {
     const { userId, items, amount, address } = req.body;
+    const { origin } = req.headers;
     const orderData = {
       userId,
       items,
@@ -44,15 +49,36 @@ const placeOrderStripe = async (req, res) => {
     const newOrder = new orderModel(orderData);
     await newOrder.save();
 
-    await userModel.findByIdAndUpdate(userId, { cartData: {} });
+    const line_items = items.map((item) => ({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.quantity,
+    }));
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency: "usd",
-      payment_method_types: ["card"],
+    line_items.push({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: "Delivery Charge",
+        },
+        unit_amount: deliveryCharge * 100,
+      },
+      quantity: 1,
     });
 
-    res.json({ success: true, message: "Order Placed", paymentIntent });
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+      line_items,
+      mode: "payment",
+    });
+
+    res.json({ success: true, session_url: session.url });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
